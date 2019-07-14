497f719416b4c96f40d0e10f084a487a02006bad
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-14 23:37:13 +0100

    Add onChange to input value and define onInputChange callback function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { searchTerm: 'fvfvfvf' };
+
+    onInputChange = () => {
+
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Search</label>
-                        <input type="text" />
+                        <input type="text" value={this.state.searchTerm} onChange={this.onInputChange}/>
                     </div>
                 </form>
             </div>
