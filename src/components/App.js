064889305a8486d4b1d7efca4908752108d2a076
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-17 23:58:25 +0100

    Make onTermSubmit an async function that awaits get reqs and set state to empty array

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,22 @@ import youtube from '../apis/youtube';
 // const API_KEY =`${process.env.REACT_APP_API_KEY}`;
 
 class App extends React.Component {
-	onTermSubmit = term => {
-		youtube.get('/search', {
+	state = { videos: [] };
+	onTermSubmit = async term => {
+		const response = await youtube.get('/search', {
 			params: {
 				q: term
 			}
 		});
+
+		this.setState({ videos: response.data.items });
 	};
 
     render() {
         return (
             <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <SearchBar onFormSubmit={ this.onTermSubmit } />
+				I have {this.state.videos.length} videos.
             </div>
         );
     }
