69107f06e38c8bae7f3d67a1ce1a0295484f18b5
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-20 15:51:58 +0100

    Add some custom CSS styling to VideoList and VideoItemwith semantic ui and a video-item custom css class

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,13 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({video}) => { // destructuring. Instead of having to call props.video, curly braces does it for us!
 	return (
-		<div>
-			<img src={video.snippet.thumbnails.medium.url} alt="" />
-			{video.snippet.title}
+		<div className="video-item item">
+			<img className="ui image" alt="" src={video.snippet.thumbnails.medium.url} />
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+			</div>
 		</div>
 	);
 };
