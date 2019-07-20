6eb7f39365906bd052c825cd9d658248be340c7d
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-20 17:42:07 +0100

    Pass VideoDetail prop into VideoDetail component from App and have a loading check mark if video is not there

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 // const API_KEY =`${process.env.REACT_APP_API_KEY}`;
 
 class App extends React.Component {
@@ -23,7 +24,8 @@ class App extends React.Component {
     render() {
         return (
             <div className="ui container">
-                <SearchBar onFormSubmit={ this.onTermSubmit } />
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
