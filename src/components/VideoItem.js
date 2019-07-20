5884cae2a23c859140671458da1dcce5d46efb05
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-20 15:24:03 +0100

    Create a div in VideoItem that returns an image and title from the YT api

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-	return <div>Video Item</div>
+const VideoItem = ({video}) => { // destructuring. Instead of having to call props.video, curly braces does it for us!
+	return (
+		<div>
+			<img src={video.snippet.thumbnails.medium.url} alt="" />
+			{video.snippet.title}
+		</div>
+	);
 };
 
 export default VideoItem;
