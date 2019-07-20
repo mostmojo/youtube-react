2babf26847277ea6965c0921e027e6ceba40bbcc
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-20 15:00:17 +0100

    Iterate through videos array and return a video item

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
+const VideoList = ({ videos }) => {
 	// props.videos
-	return <div>{props.videos.length}</div>;
+	const renderedList = videos.map((video) => {
+		return <VideoItem />;
+	});
+	return <div>{renderedList}</div>;
 };
 
 export default VideoList;
