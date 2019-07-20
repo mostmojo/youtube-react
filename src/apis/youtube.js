feee0ad30e3be4a19711704d83192bbc7aa28ce8
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-20 19:25:39 +0100

    Fix bug

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDl7fcFF2lOpKBozK6GkmWXRo2JVaFa27U";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
